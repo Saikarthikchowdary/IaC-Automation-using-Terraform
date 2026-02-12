259772875c200bb531e6f506d7650174029e4d14
Sai Karthik <113954235+Saikarthikchowdary@users.noreply.github.com>
2026-02-12 10:51:14 +0530

    Terraform backend edited

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "cloud-devops-pro"
-    key    = "jenkins/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "saikarthik-tf-state-2026"
+    key            = "jenkins/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
