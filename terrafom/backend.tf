b6527810bf9572ce7fb5004691098f10f784547c
Saikarthik <saikarthikgarapati@gmail.com>
2026-02-15 16:27:36 +0530

    restructured the files

--- terrafom/backend.tf
+++ terrafom/backend.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "s3" {
-<<<<<<< HEAD
     bucket         = "saikarthik-tf-state-2026"
-=======
-    bucket         = "your-new-unique-bucket"
->>>>>>> cb1ed8a (refactor: remove local terraform artifacts and old jenkins install script)
     key            = "jenkins/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
